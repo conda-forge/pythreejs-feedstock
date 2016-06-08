aee4a538fb0d3aad99e4f01fc3adab8712d74979
Nicholas Bollweg <nbollweg@continuum.io>
2016-06-08 18:05:29 -0500

    squelch link script output

--- recipe/pre-unlink.bat
+++ recipe/pre-unlink.bat
@@ -1,1 +1,1 @@
-"%PREFIX%\Scripts\jupyter-nbextension.exe" disable pythreejs --py --sys-prefix && if errorlevel 1 exit 1
+"%PREFIX%\Scripts\jupyter-nbextension.exe" disable pythreejs --py --sys-prefix > /dev/null 2>&1 && if errorlevel 1 exit 1
